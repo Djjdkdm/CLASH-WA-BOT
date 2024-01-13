b050bf23231006eb3da28db48eb4cea749c18cba
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2024-01-13 18:23:32 +0530

    Update converter.js

--- components/converter.js
+++ components/converter.js
@@ -9,3 +9,10 @@ var language = args.split(",")[1];
 const audiourl = googleTTS.getAudioUrl(text, {lang: language, slow: false, host: 'https://translate.google.com',});
 await conn.sendMessage(msg.from,{audio:{url:audiourl},mimetype: 'audio/mpeg'},{quoted:msg});
 });
+
+clash({pattern: "photo", fromMe: false, desc: "Converts sticker to photo.", type: "converter",},
+async ({args, msg, conn}) => {
+if(!msg.isQuotedMsg) return await msg.tinyreply("*_Reply to a sticker!_*");
+let buff = await msg.quotedMsg.download();
+return await conn.sendMessage(msg.from,{image:{url:buff}},{quoted:msg});
+});
