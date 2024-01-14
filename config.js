f6323adecd4dc6d8702e312531b6f5d7c9b5c40d
Djjdkdm <127647216+Djjdkdm@users.noreply.github.com>
2024-01-14 11:55:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = {
   OWNER_NUMBER: ["919961857267@s.whatsapp.net", "0@s.whatsapp.net"],
   STICKER_DATA: process.env.STICKER_DATA || "CLASH-WA-BOT;TOXIC-KICHUX",
   SUDO: process.env.SUDO || "919961857267,0",
-  API: process.env.API || "https://api-viper.onrender.com", //don't change this link. If you change then some commands will not work!.
+  API: process.env.API || "https://api-viper-x.koyeb.app", //don't change this link. If you change then some commands will not work!.
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   OWNER_NAME: process.env.OWNER_NAME || "TOXIC-KICHU",
